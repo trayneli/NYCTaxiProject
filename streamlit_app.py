a0fa4b2783444cc1926089e5dd4ca5956ec6747d
trayneli <43825648+trayneli@users.noreply.github.com>
2025-11-22 15:30:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("NYC Taxi Analytics Dashboard")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "NYC Efficiency Analysis"
 )
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(layout="wide")
+
+tableau_url = "https://public.tableau.com/views/NYCTaxiEfficiency/Dashboard3"
+
+embed_url = f"{tableau_url}?:language=en-US&:display_count=n&:origin=viz_share_link&:embed=yes&:showVizHome=no&:toolbar=top"
+
+components.iframe(embed_url, height=1500, scrolling=True)
