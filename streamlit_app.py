4fb3a9d02844443c685874aecc156fb7c4a8b029
trayneli <43825648+trayneli@users.noreply.github.com>
2025-11-23 19:00:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,10 @@ st.set_page_config(layout="wide")
 st.title("NYC Taxi Analytics Dashboard")
 
 tableau_url = "https://public.tableau.com/views/NYCTaxiEfficiency/Dashboard2?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
-
+tableau_url2="https://public.tableau.com/views/Taxi_Demand_and_Vendor_EDA_Final/FinalDashboard?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
 
 embed_url = f"{tableau_url}&:embed=yes&:showVizHome=no&:toolbar=top"
+embed_url2 = f"{tableau_url2}&:embed=yes&:showVizHome=no&:toolbar=top"
 
 ## Pages for dashboards
 tab1, tab2, tab3, tab4 = st.tabs(["EDA", "Efficiency Analysis", "Taxi Ride Analysis", "Taxi Predictions"])
@@ -21,7 +22,7 @@ with tab1:
 with tab2:
     st.markdown("### Efficiency Analysis")
     st.caption("Revenue trends and sales performance")
-    components.iframe(embed_url, width=1200, height=1500, scrolling=True)
+    components.iframe(embed_url2, width=1200, height=1500, scrolling=True)
 
 with tab3:
     st.markdown("### Taxi Ride Analysis")
